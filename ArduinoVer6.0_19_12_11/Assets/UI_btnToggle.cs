248d05cba644145d133fdd88711a286d532bb9ec
SEOSEOKGWON <tmxk061@naver.com>
2019-12-20 10:23:02 +0900

    ex

---
+++ ArduinoVer6.0_19_12_11/Assets/UI_btnToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_btnToggle : MonoBehaviour
+{
+    [SerializeField]
+    private Image nextTarget;
+    [SerializeField]
+    private Text nextText;
+
+    [SerializeField]
+    private Image thisTarget;
+    [SerializeField]
+    private Text thisText;
+
+
+    public void OnClick()
+    {
+        nextTarget.enabled = true;
+        nextText.enabled = true;
+
+        thisTarget.enabled = false;
+        thisText.enabled = false;
+    }
+}
