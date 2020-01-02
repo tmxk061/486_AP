2e9dd6b24632ba3acaf7fb82b29d0ca8137f4074
wkdwlstj123 <56428562+wkdwlstj123@users.noreply.github.com>
2020-01-02 10:21:46 +0900

    newmap

---
+++ ArduinoVer6.0_19_12_11/Assets/AutoDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour
+{
+    //public GameObject door;
+    Animator animator;
+    // bool autodoor;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerStay(Collider col)
+    { 
+        if (col.gameObject.tag == "Player")
+        {
+ 
+            animator.SetBool("AutoDoor", true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        //if (col.gameObject.tag == "Player")
+        //{
+            Debug.Log("In");
+            animator.SetBool("AutoDoor", false);
+        //}
+    }
+}
