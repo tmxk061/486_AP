3f33ccb1c561d19a8253c440f895856176ad92b6
SEOSEOKGWON <tmxk061@naver.com>
2020-01-02 14:44:00 +0900

    ex

---
+++ ArduinoVer6.0_19_12_11/Assets/Mug_cup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mug_cup : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject Water;
+
+    float distance = 10;
+    public bool waterCheck = false;
+    public bool MouseClick = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "water" && waterCheck == false)
+        {
+            Water.SetActive(true);
+        }
+    }
+
+    #region MouseDrag
+
+    private void OnMouseDown()
+    {
+        Debug.Log(this.transform.position.z - Camera.main.transform.position.z);
+        
+        distance = Vector3.Distance(this.transform.position, Camera.main.transform.position);
+
+    }
+
+    private void OnMouseUp()
+    {
+        MouseClick = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        MouseClick = true;
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            distance -= 10;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            distance += 10;
+        }
+
+        if (this.gameObject.layer == LayerMask.NameToLayer("Sensor"))
+        {
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x,
+                                                Input.mousePosition.y, distance);
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
+
+        }
+
+    }
+
+    #endregion MouseDrag
+}
