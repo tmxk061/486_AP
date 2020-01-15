f334532d746bbd912514405632aa92b79cc00359
SEOSEOKGWON <tmxk061@naver.com>
2020-01-15 13:24:27 +0900

    퀵무브 준비

---
+++ ArduinoVer6.0_19_12_11/Assets/QuickMove_Mgr.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuickMove_Mgr : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject QuickMoveUI;
+
+    [SerializeField]
+    private Animator ani;
+
+    [SerializeField]
+    private GameObject blockCodingBtn;
+
+    private bool isOn = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!isOn)
+            {
+                isOn = true;
+                QuickMoveUI.SetActive(true);
+            }
+            else if(isOn)
+            {
+                isOn = false;
+                ani.SetTrigger("off");
+                Invoke("ActiveeFalse", 0.5f);
+            }
+        }
+    }
+
+    private void ActiveeFalse()
+    {
+        QuickMoveUI.SetActive(false);
+    }
+
+    public void BlockCordingOnclick()
+    {
+        isOn = false;
+        ActiveeFalse();
+        blockCodingBtn.GetComponent<OnCubeClick>().OnMouseDown();
+
+    }
+
+}
