3ec3b8e361bc28fcf064a19fd81bf0b13deffea5
wkdwlstj123 <56428562+wkdwlstj123@users.noreply.github.com>
2020-01-16 16:14:09 +0900

    sss

---
+++ ArduinoVer6.0_19_12_11/Assets/EXwater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXwater : MonoBehaviour
+{
+    public WaterValue wv;     
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (wv.AnalogConnect == true && wv.GNDConnect == true && wv.VccConnect == true)
+        {
+            Debug.Log(wv.value);
+        }
+    }
+}
