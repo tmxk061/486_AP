fc688ee102d47df4fd775a941ec5e8bf4515d437
wkdwlstj123 <56428562+wkdwlstj123@users.noreply.github.com>
2020-01-16 14:45:19 +0900

    Revert "sss"
    This reverts commit 0707d4a5321d82d39a2cff044b0516cb1c042d1f.

--- ArduinoVer6.0_19_12_11/Assets/usetoilet.cs
+++ ArduinoVer6.0_19_12_11/Assets/usetoilet.cs
@@ -5,71 +5,42 @@ using UnityEngine;
 public class usetoilet : MonoBehaviour
 {
     Animator animator;
-
-    public WaterValue wv;
-    public GameObject Green;
-
-    [SerializeField]
-    float dely;
-    public float maxdely;
-
-    bool use;
+     bool use;
 
     void Start()
     {
-        dely = -5;
-
-        animator = GetComponent<Animator>();
         use = true;
 
-        animator.SetBool("TurnOn", true);
-    }
+        animator = GetComponent<Animator>();
 
-    private void Update()
-    {
-
-        dely -= Time.deltaTime * 1f;
-
-        if (-2 <= dely && dely <= 0)
-        {
-            use = true;
-            animator.SetBool("TurnOn", true);
-        }
+        toiltrue();
     }
 
     private void OnMouseDown()
     {
-        if (wv.AnalogConnect == true && wv.GNDConnect == true && wv.VccConnect == true && Green.activeSelf == true)
+        Debug.Log("1111");
+
+        if (use == true)
         {
-            if (use == true)
-            {
-                animator.SetBool("TurnOn", false);
-                use = false;
+            use = false;
+            toifalse();
 
-                dely = maxdely;
-                return;
-            }
         }
+        else if (use == false)
+        {
+            use = true;
+            toiltrue();
+        }
+    }
 
-        //else if (use == false)
-        //{
-        //    Debug.Log("bbb");
-        //    animator.SetBool("TurnOn", true);
-        //    use = true;
+    private void toiltrue()
+    {
+        animator.SetBool("TurnOn", true);
+    }
 
-        //    return;
-        //}
+    private void toifalse()
+    {
+        animator.SetBool("TurnOn", false);
 
-        //if (Green.activeSelf == true)
-        //{
-        //    Debug.Log("in");
-        //    if (use == true)
-        //    {
-        //        Debug.Log("in2");
-        //        use = false;
-        //        animator.SetBool("TurnOn", false);
-        //    }
-        //    animator.SetBool("TurnOn", true);
-        //}
     }
 }
