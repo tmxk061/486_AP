e817f7f7dfada12767f5306eb9c133848ba4f98c
SEOSEOKGWON <tmxk061@naver.com>
2020-02-10 10:25:23 +0900

    튜토리얼 초안

--- ArduinoVer6.0_19_12_11/Assets/TutorialMgr.cs
+++ ArduinoVer6.0_19_12_11/Assets/TutorialMgr.cs
@@ -1,64 +1,105 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class TutorialMgr : MonoBehaviour
 {
     [SerializeField]
-    public GameObject TextBox;
+    public GameObject TutorialObj; //전체 튜토리얼 오브젝트를 담은 오브젝트
 
     [SerializeField]
-    public Text TextBoxText;
+    public GameObject TextBox; // 텍스트 박스 UI
 
+    [SerializeField]
+    public Text TextBoxText;//텍스트 박스의 텍스트
+
+    private int Flow;
     private int FlowNum;
+
+    private bool isStart = false;
+
+    public void StartTutorial(int i)
+    {
+        TutorialObj.SetActive(true);
+        Flow = i;
+        FlowNum = 1;
+        isStart = true;
+    }
+
+    public void EndTutorial()
+    {
+        TutorialObj.SetActive(false);
+
+        FlowNum = 0;
+        Flow = 0;
+        isStart = false;
+    }
 
     private void Start()
     {
-        StartCoroutine(TutorialRogic());
+        StartTutorial(1);
     }
 
-
-    IEnumerator TutorialRogic()
+    private void Update()
     {
-        while (true)
+        if (isStart)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                FlowNum++;
-                TutorialFlow_1();
-            }
+            TutorialFlow();
         }
     }
 
-  
-    public void TutorialFlow_1()
+    private void TutorialFlow()
+    {
+        switch (Flow)
+        {
+            case 1:
+                Tutorial_1();
+                break;
+        }
+    }
+
+    
+
+    private void Tutorial_1()
     {
         switch (FlowNum)
         {
             case 1:
                 SetTextBox("튜토리얼을 테스트 하는 중입니다.");
+                NextFlow(1);
                 break;
+
             case 2:
                 SetTextBox("튜토리얼이 잘 보이시나요?");
+                NextFlow(1);
                 break;
+
             case 3:
                 SetTextBox("그렇다면 다행이긴한데");
+                NextFlow(1);
                 break;
+
             default:
-                resetFlowNum();
+                EndTutorial();
                 break;
         }
     }
 
+    private void NextFlow(int i)
+    {
+        switch (i)
+        {
+            case 1: //마우스 왼쪽 클릭
+                if (Input.GetMouseButtonDown(0))
+                {
+                    FlowNum++;
+                }
+                break;
+        }
+
+    } //다음 흐름으로 넘어가는 조건
 
     private void SetTextBox(string msg)
     {
         TextBoxText.text = msg;
-    }
-
-    private void resetFlowNum()
-    {
-        FlowNum = 0;
     }
 }
