2ed0d311042fb0e5f502e0e408bcf9c7e41da031
SEOSEOKGWON <tmxk061@naver.com>
2020-02-10 15:52:39 +0900

    무브트리거

---
+++ ArduinoVer6.0_19_12_11/Assets/01.Scripts/Tutorial/TutorialMovePoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMovePoint : MonoBehaviour
+{
+    [SerializeField]
+    private TutorialTrigger trigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            trigger.OnTutorialTriggerOn();
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ ArduinoVer6.0_19_12_11/Assets/01.Scripts/Tutorial/TutorialTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public int trigerNumber;
+    public bool used = false;
+
+    public void OnTutorialTriggerOn()
+    {
+        if (TutorialMgr.instance.isStart &&
+            TutorialMgr.instance.OutTrigger == trigerNumber &&
+            !used)
+        {
+            used = true;
+            TutorialMgr.instance.FlowNum++;
+        }
+    }
+}
