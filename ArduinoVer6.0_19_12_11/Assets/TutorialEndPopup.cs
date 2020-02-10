3370d2e511ee99b5762f830b00260b8b40540116
SEOSEOKGWON <tmxk061@naver.com>
2020-02-10 20:56:20 +0900

    기본 튜토리얼 흐름 완성

---
+++ ArduinoVer6.0_19_12_11/Assets/TutorialEndPopup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEndPopup : MonoBehaviour
+{
+    public int trigerNumber;
+    public bool used = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnTutorialTriggerOn();
+    }
+
+    public void OnTutorialTriggerOn()
+    {
+        if (TutorialMgr.instance.isStart &&
+            TutorialMgr.instance.OutTrigger == trigerNumber &&
+            !used)
+        {
+            used = true;
+            TutorialMgr.instance.FlowNum++;
+        }
+    }
+}
