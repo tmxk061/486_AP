ef565116fe0500b0968a87d7f2cb540c0d344c6f
wkdwlstj123 <56428562+wkdwlstj123@users.noreply.github.com>
2020-02-11 14:52:31 +0900

    www

---
+++ ArduinoVer6.0_19_12_11/Assets/MAllDelete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAllDelete : MonoBehaviour
+{
+    GameObject[] Sensor;
+
+    public ConfirmSensor cs;
+
+    public void FandDSensor()
+    {
+        Sensor = GameObject.FindGameObjectsWithTag("Sensor");
+
+        try
+        {
+            for (int i = 0; i < Sensor.Length; i++)
+            {
+                Destroy(Sensor[i]);
+            }
+        }
+        catch
+        {
+
+        }
+
+        cs.DeleteAnser();
+    }
+}
