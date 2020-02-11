39484dc2cba1393215023e3b2ccc2c2be7fc5c73
wkdwlstj123 <56428562+wkdwlstj123@users.noreply.github.com>
2020-02-11 21:48:08 +0900

    AAA

---
+++ ArduinoVer6.0_19_12_11/Assets/BAllDelete.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BAllDelete : MonoBehaviour
+{
+    GameObject[] UltBlcok;
+    GameObject[] DigitalWrite;
+    GameObject[] ifBlcok;
+    GameObject[] WaitBlock;
+    GameObject[] ServoBlock;
+    GameObject[] WaterMachineBlock;
+    GameObject[] AnalogRead;
+    GameObject[] WhileBlock;
+
+    public GameObject NewStart;
+
+    public void DeleteBlock()
+    { 
+        try
+        {
+            UltBlcok = GameObject.FindGameObjectsWithTag("UltBlock");
+            DigitalWrite = GameObject.FindGameObjectsWithTag("DigitalWrite");
+            ifBlcok = GameObject.FindGameObjectsWithTag("ifBlock");
+            WaitBlock = GameObject.FindGameObjectsWithTag("WaitBlock");
+            ServoBlock = GameObject.FindGameObjectsWithTag("ServoBlock");
+            WaterMachineBlock = GameObject.FindGameObjectsWithTag("WaterMachineBlock");
+            AnalogRead = GameObject.FindGameObjectsWithTag("AnalogRead");
+            WhileBlock = GameObject.FindGameObjectsWithTag("whileBlock");
+           
+            for (int i = 0; i < UltBlcok.Length; i++)
+            {
+                Destroy(UltBlcok[i]);
+            }
+           
+            for (int i = 0; i < DigitalWrite.Length; i++)
+            {
+                Destroy(DigitalWrite[i]);
+            }
+           
+            for (int i = 0; i < ifBlcok.Length; i++)
+            {
+                Destroy(ifBlcok[i]);
+            }
+           
+            for (int i = 0; i < WaitBlock.Length; i++)
+            {
+                Destroy(WaitBlock[i]);
+            }
+           
+            for (int i = 0; i < ServoBlock.Length; i++)
+            {
+                Destroy(ServoBlock[i]);
+            }
+           
+            for (int i = 0; i < WaterMachineBlock.Length; i++)
+            {
+                Destroy(WaterMachineBlock[i]);
+            }
+           
+            for (int i = 0; i < AnalogRead.Length; i++)
+            {
+                Destroy(AnalogRead[i]);
+            }
+           
+            for (int i = 0; i < WhileBlock.Length; i++)
+            {
+                Destroy(WhileBlock[i]);
+            }     
+        }
+        catch
+        {
+
+        }
+    }
+}
---
+++ ArduinoVer6.0_19_12_11/Assets/DStartBlock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DStartBlock : MonoBehaviour
+{
+    void Start()
+    {
+        this.gameObject.SetActive(false);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
