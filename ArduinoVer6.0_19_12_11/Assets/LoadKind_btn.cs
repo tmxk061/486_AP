39bdca4f0d30305286e48b7a8fa4a488dd730137
SEOSEOKGWON <tmxk061@naver.com>
2020-02-19 14:34:12 +0900

    준비준비

---
+++ ArduinoVer6.0_19_12_11/Assets/LoadKind_btn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadKind_btn : MonoBehaviour
+{
+    public GameObject LocalUI;
+    public GameObject ServerUI;
+
+
+    public void OnKindBtnClick(int i)
+    {
+        if (i == 0)
+        {
+            LocalUI.SetActive(true);
+            ServerUI.SetActive(false);
+
+        }
+        else
+        {
+            LocalUI.SetActive(false);
+            ServerUI.SetActive(true);
+        }
+    }
+
+}
---
+++ ArduinoVer6.0_19_12_11/Assets/Net_CatalogBtn.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Net_CatalogBtn : MonoBehaviour
+{
+    public string KEY;
+    public string NAME;
+    public string DATE;
+}
