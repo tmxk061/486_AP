8900d4de491975673b3bb84bde6f28ea1de0a55d
waaa16 <kimdg4738@gmail.com>
2020-02-20 13:44:03 +0900

    UI, Connect Add

---
+++ ArduinoVer6.0_19_12_11/Assets/Load_BlockMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Load_BlockMenu : MonoBehaviour
+{
+    public List<string[]> Main_Data = new List<string[]>();
+
+    [SerializeField]
+    private GameObject Btn; //생성할 버튼
+
+    [SerializeField]
+    private Transform parent; //버튼 생성될부모
+
+    List<GameObject> BtnList = new List<GameObject>();
+
+    private void OnEnable()
+    {
+        ContentClear();
+        Main_Data.Clear();
+        List<string[]> tempData = new List<string[]>();
+        BlockSave.instance.GetBlockCode_DB(Main_Data);
+        Debug.Log(Main_Data.Count);
+        for (int i = 0; i < Main_Data.Count; i++)
+        {
+            GameObject newbtn = Instantiate(Btn);
+            newbtn.SetActive(true);
+            newbtn.transform.parent = parent;
+            newbtn.transform.position = parent.position;
+            newbtn.transform.localScale = Btn.transform.localScale;
+
+            //newbtn.GetComponent<StudyBtn>().ID = int.Parse(Main_Data[i][0]);
+            Debug.Log(Main_Data[i][0] + ", " + Main_Data[i][1]);
+            newbtn.GetComponentInChildren<Text>().text = Main_Data[i][0] + " , " + Main_Data[i][1];
+            newbtn.GetComponent<Load_BlockBtn>().NAME = Main_Data[i][0];
+            newbtn.GetComponent<Load_BlockBtn>().DATE = Main_Data[i][1];
+
+            BtnList.Add(newbtn);
+        }
+    }
+
+
+    private void ContentClear()
+    {
+        for (int i = 0; i < BtnList.Count; i++)
+        {
+            GameObject.Destroy(BtnList[i]);
+        }
+        BtnList.Clear();
+    }
+}
---
+++ ArduinoVer6.0_19_12_11/Assets/Net_BlockCatalogBtn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Net_BlockCatalogBtn : MonoBehaviour
+{
+    //public string KEY;
+    public string NAME;
+    public string DATE;
+    public string realData;
+
+    public void OnNetworkLoad()
+    {
+        List<List<string>> Alldata = new List<List<string>>();
+
+        string TargetData = realData;
+        TargetData = TargetData.Substring(1, TargetData.Length - 1);
+        TargetData = TargetData.Substring(0, TargetData.Length - 1);
+        Debug.Log(TargetData);
+        string[] target = TargetData.Split(new char[] { '@' });
+
+        for (int i = 0; i < target.Length; i++)
+        {
+            List<string> data = new List<string>();
+
+            string d = target[i].Substring(0, target[i].Length - 1);
+            string[] smallParsing = d.Split(new char[] { '$' });
+            Debug.Log(smallParsing.Length);
+
+            for (int a = 0; a < smallParsing.Length; a++)
+            {
+                data.Add(smallParsing[a]);
+            }
+
+            Alldata.Add(data);
+        }
+
+        GameObject.Find("BlockSaveBtn").GetComponent<BlockSave>().Load_Net(Alldata);
+        GameObject.Find("close").GetComponent<Menu_Data_Close>().OnBtnClick();
+
+    }
+
+}
