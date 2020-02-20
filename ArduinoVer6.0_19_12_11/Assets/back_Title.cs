2cc08a67d520e9809a41d3c888192bd9e30c87f0
SEOSEOKGWON <tmxk061@naver.com>
2020-02-20 16:57:27 +0900

    ㅇㅇ

---
+++ ArduinoVer6.0_19_12_11/Assets/back_Title.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back_Title : MonoBehaviour
+{
+    public void OnBackTitleBtn()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
---
+++ ArduinoVer6.0_19_12_11/Assets/title_screen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title_screen : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LoadingSceneManager.LoadScene("Main_Stage");
+        }
+        //SceneManager.LoadScene("Main_Stage");
+       
+    }
+}
